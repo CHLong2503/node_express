ec3abf5c94048f06c8bf82922f1c9ddecfb6bfcb
CHLong2503 <caolong2503@gmail.com>
2023-12-06 18:00:20 +0700

    update cloudinaryConfigs.js

--- src/configs/cloudinaryConfig.js
+++ src/configs/cloudinaryConfig.js
@@ -1,9 +1,15 @@
 import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+dotenv.config();
+
+const CLOUD_NAME = process.env.CLOUD_NAME;
+const API_KEY = process.env.API_KEY;
+const API_SECRET = process.env.API_SECRET;
 
 cloudinary.config({
-  cloud_name: "dfhamhcp5",
-  api_key: "469653455638959",
-  api_secret: "iEqtYI1yNLvBLC_g4MuirYI1Xhk",
+  cloud_name: CLOUD_NAME,
+  api_key: API_KEY,
+  api_secret: API_SECRET,
 });
 
 export default cloudinary;
